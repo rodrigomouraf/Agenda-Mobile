acab2848a77c230d6cf2de2c7aed7b9cf0ab08a8
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2020-02-23 02:22:41 -0300

    aula 05 Finalizando o Fluxo do APP

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,12 +1,8 @@
-package br.com.alura.agenda;
+package br.com.alura.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,19 +11,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
+
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        List<String> alunos = new ArrayList<>(
-                Arrays.asList("Jean","Robson","Anna","Lucas","Rodrigo")
-        );
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        setContentView(R.layout.activity_lista_alunos);
+
+        AlunoDAO dao = new AlunoDAO();
+
+
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_expandable_list_item_1,
-                alunos));
+                dao.todos()));
 
     }
 }
